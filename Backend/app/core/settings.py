22ed77c0f7517456f1127c6f70ca257a95254061
I-Abdullah-I <abdullaashraf1998@gmail.com>
2021-12-11 20:10:21 +0200

    Final V.0.1

--- Backend/app/core/settings.py
+++ Backend/app/core/settings.py
@@ -104,22 +104,16 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    #  "default": {
-    #     "ENGINE": 'djongo',
-    #     "NAME": 'e_store_db',
-    #     "ENFORCE_SCHEMA": False,
-    #     "CLIENT": {
-    #         "HOST": 'db',
-    #     }
-    # }
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
-    }
+DATABASES = {​​​
+     "default": {​​​
+        "ENGINE": 'djongo',
+        "NAME": 'e_store_db',
+        "ENFORCE_SCHEMA":False,
+        "CLIENT": {​​​
+            "host": "db:26061",
+        }​​​
+
+    }​​​
 }
 
 
